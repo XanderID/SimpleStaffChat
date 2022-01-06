4a98074db76b6d0930959186452c4798ba89c183
MulqiGaming64 <mulqi311@gmail.com>
2022-01-06 13:56:32 +0700

    Create SimpleStaffChatCommands.php

---
+++ src/MulqiGaming64/SimpleStaffChat/Commands/SimpleStaffChatCommands.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MulqiGaming64\SimpleStaffChat\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\player\Player;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\utils\TextFormat;
+
+use MulqiGaming64\SimpleStaffChat\SimpleStaffChat;
+
+class SimpleStaffChatCommands extends Command implements PluginOwned {
+
+	/** @var SimpleStaffChat $plugin */
+    private $plugin;
+
+    public function __construct(SimpleStaffChat $plugin) {
+        $this->plugin = $plugin;
+        parent::__construct("simplestaffchat", "a Menu for Staff Administrator", "/sschat", ["sschat", "schat", "sc"]);
+        $this->setPermission("simplestaffchat.use");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) : bool {
+        if (!$this->testPermission($sender)) return false;
+        if($this->getOwningPlugin()->setStaffChat($sender)){
+        	$sender->sendMessage("§aStaffChat successfully Enabled");
+        } else {
+        	$sender->sendMessage("§aStaffChat successfully Disabled");
+        }
+        return true;
+    }
+
+    public function getOwningPlugin(): SimpleStaffChat{
+        return $this->plugin;
+    }    
+}
